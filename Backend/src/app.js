19c4f7fdfaa7f615395dbbfb7a446443598e8d2e
Anuj <anujlokhande25@gmail.com>
2026-01-02 23:31:05 +0530

    cors updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,7 +1,14 @@
 import express, { urlencoded } from "express";
 import cors from "cors";
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://contact-management-system-64msf9017-anujlokhandes-projects.vercel.app/",
+    ],
+    credentials: true,
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
